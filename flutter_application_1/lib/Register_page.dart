23c9a980d27828e35faff8767b1a1b1ce4d32921
u6488197 <143207489+u6488197@users.noreply.github.com>
2024-02-25 16:39:18 +0700

    Update Register_page.dart

--- flutter_application_1/lib/Register_page.dart
+++ flutter_application_1/lib/Register_page.dart
@@ -1,1 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Category',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RegisterPage(title: 'CoachCook Category Layout'),
+    );
+  }
+}
+
+class RegisterPage extends StatelessWidget {
+  RegisterPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  final _formKey = GlobalKey<FormBuilderState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(200.0),
+        child: AppBar(
+          flexibleSpace: Container(
+            padding: EdgeInsets.symmetric(vertical: 40.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Coach',
+                      style: TextStyle(
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFFFFDE59),
+                      ),
+                    ),
+                    Text(
+                      'Cook',
+                      style: TextStyle(
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+          child:Container(
+            padding: EdgeInsets.all(10.0),
+            child: FormBuilder(
+              key: _formKey,
+              child: Column(
+                children: [
+                  FormBuilderTextField(
+                    name: 'username',
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      labelStyle: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),),
+                    validator: FormBuilderValidators.required(),
+                  ),
+                  SizedBox(height: 20.0),
+                  FormBuilderTextField(
+                    name: 'password',
+                    style: TextStyle(fontSize: 20.0),
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    obscureText: true,
+                    validator: FormBuilderValidators.required(),
+                  ),
+                  SizedBox(height: 20.0),
+                  FormBuilderTextField(
+                    name: 'confirmPassword',
+                    style: TextStyle(fontSize: 20.0),
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      labelStyle: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value != _formKey.currentState?.fields['password']?.value) {
+                        return 'Passwords do not match';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.yellow,
+                    ),
+                    onPressed: () {
+
+                    },
+                    child: Text('create account'),
+                  ),
+                ],
+              ),
+            ),
+      )
+      ),
+    );
+  }
+}
