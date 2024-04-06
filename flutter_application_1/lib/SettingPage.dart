8344d05890ee5ed9c126a68eacf5d3732e8400d5
Nattarat11 <nattarat.pan@student.mahidol.edu>
2024-04-06 17:13:04 +0700

    Added SettingPage

---
+++ flutter_application_1/lib/SettingPage.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SettingPage(),
+        '/SettingPage': (context) => const SettingPage(),
+      },
+    );
+  }
+}
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(200.0),
+        child: AppBar(
+          flexibleSpace: Container(
+            padding: EdgeInsets.symmetric(vertical: 40.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Coach',
+                      style: TextStyle(
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFFFFDE59),
+                      ),
+                    ),
+                    Text(
+                      'Cook',
+                      style: TextStyle(
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Icon(
+              Icons.lock,
+              size: 36, // Adjust the icon size
+            ),
+            title: Text(
+              'Change Password',
+              style: TextStyle(fontSize: 20), // Adjust the text size
+            ),
+            onTap: () {
+              // Navigate to change password page
+              Navigator.pushNamed(context, '/ChangePasswordPage');
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.logout,
+              size: 36, // Adjust the icon size
+            ),
+            title: Text(
+              'Logout',
+              style: TextStyle(fontSize: 20), // Adjust the text size
+            ),
+            onTap: () {
+              // Perform log out action
+              // For example, you can clear user session and navigate to login page
+              Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
