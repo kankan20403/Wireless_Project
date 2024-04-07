10858e67994c899a0e466dafa1663915fd696d1e
u6488197 <143207489+u6488197@users.noreply.github.com>
2024-04-08 00:23:28 +0700

    Update ChangePassword.dart

--- flutter_application_1/lib/ChangePassword.dart
+++ flutter_application_1/lib/ChangePassword.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Menu',
+      title: 'Change Password',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
