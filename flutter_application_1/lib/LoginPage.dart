39b7678378d4878d06f6695b2c3681d22e9fbcb5
u6488197 <143207489+u6488197@users.noreply.github.com>
2024-04-08 00:28:20 +0700

    Update LoginPage.dart

--- flutter_application_1/lib/LoginPage.dart
+++ flutter_application_1/lib/LoginPage.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Category',
+      title: 'Log In',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
