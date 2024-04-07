cbb00203be684eea0584fbe958d3ccabe5cf9e52
u6488197 <143207489+u6488197@users.noreply.github.com>
2024-04-08 00:39:33 +0700

    Update Register_page.dart

--- flutter_application_1/lib/Register_page.dart
+++ flutter_application_1/lib/Register_page.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    title: 'Category',
+    title: 'Register',
     theme: ThemeData(
     primarySwatch: Colors.blue,
     ),
